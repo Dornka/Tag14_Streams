ab6ebba4637b998706060e1f3fa628f83fbbb373
Dornka <Skiber@gmail.com>
2023-09-07 10:52:31 +0200

    added .map doubledNumbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,32 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+//      Schritt 1: Filtert eine Liste von Zahlen und gebt nur die geraden Zahlen aus.
 
+//      Schritt 2: Verwendet 'map' und verdoppelt jede Zahl in der Liste.
+
+//      Schritt 3: Sortiert die Liste in aufsteigender Reihenfolge.
+
+//      Schritt 4: Führt eine 'reduce'-Operation durch, um die Summe aller Zahlen in der Liste zu berechnen.
+
+//      Schritt 5: Nutzt 'forEach' und gebt jede verarbeitete Zahl aus.
+
+//      Schritt 6: Sammelt die verarbeiteten Zahlen in einer neuen Liste mit 'collect'.
 public class Main {
     public static void main(String[] args) {
+
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         List<Integer> evenNumbers = numbers.stream()
                 .filter(n -> n % 2 == 0)
-                .collect(Collectors.toList());
-        System.out.println("Schritt 1 - Gerade Zahlen: " + evenNumbers);
+                .toList();
+        System.out.println("Gerade Zahlen: " + evenNumbers);
 
         List<Integer> doubledNumbers = numbers.stream()
                 .map(n -> n * 2)
-                .collect(Collectors.toList());
-        System.out.println("Schritt 2 - Verdoppelte Zahlen: " + doubledNumbers);
+                .toList();
+        System.out.println("Verdoppelte Zahlen: " + doubledNumbers);
 
-        List<Integer> sortedNumbers = numbers.stream()
-                .sorted()
-                .collect(Collectors.toList());
-        System.out.println("Schritt 3 - Sortierte Zahlen: " + sortedNumbers);
 
-        int sum = numbers.stream()
-                .reduce(0, Integer::sum);
-        System.out.println("Schritt 4 - Summe aller Zahlen: " + sum);
-
-        numbers.stream()
-                .forEach(n -> System.out.println("Schritt 5 - Verarbeitete Zahl: " + n));
-        List<Integer> processedNumbers = numbers.stream()
-                .map(n -> n * 2)
-                .collect(Collectors.toList());
-        System.out.println("Schritt 6 - Verarbeitete Zahlen: " + processedNumbers);
     }
 }
